e23aee69edb88f93be714d3a2ab37e26ed6b1299
chemokita13 <chemokita13@gmail.com>
2022-12-14 22:49:59 +0100

    tailwindcss added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"; // styles
+import { useState } from "react";
+import Editor, { loader } from "@monaco-editor/react"; // monaco editor react
+import * as monaco from "monaco-editor"; // monaco original repo
+
+///const { ipcRenderer } = window.require('electron') // from electronjs docs
+//import {ipcRenderer} from 'electron'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    loader.config({ monaco }); // To replace monaco-editor/react cdn's to local files
+
+    const [EditorValue, setEditorValue] = useState(""); // default value
+
+    const setHTMLpart = () => {
+        return { __html: EditorValue };
+    }; // return value
+
+    ///ipcRenderer.on('file:open', (event, msg) => setEditorValue(msg))
+
+    return (
+        <section className="content">
+            <div className="editor-part">
+                <Editor
+                    height="100%"
+                    theme="vs-dark"
+                    language="html"
+                    value={EditorValue}
+                    onChange={(newValue) => {
+                        setEditorValue(newValue);
+                    }}
+                    className="editor"
+                />
+            </div>
+            <div
+                className="renderer"
+                dangerouslySetInnerHTML={setHTMLpart()}
+            ></div>
+        </section>
+    );
 }
 
 export default App;
